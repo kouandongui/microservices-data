ac3b92ba234a617b2d2af7d1998d11616d84d769
AlexandreGuil <guillemotalexandre.eme@gmail.com>
2020-04-17 11:01:35 +0200

    add @EqualsAndHashCode(callSuper=false) on @Data in CustomerRequest.java

--- microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/rest/bean/CustomerRequest.java
+++ microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/rest/bean/CustomerRequest.java
@@ -2,11 +2,13 @@ package com.inti.formation.shop.api.rest.bean;
 
 import com.inti.formation.shop.api.repository.model.Customer;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author Sylvanius Kouandongui
  */
 
+@EqualsAndHashCode(callSuper = false)
 @Data
 public class CustomerRequest extends Customer {
 
